231df0b97a02892633fb8ad739a1bb6ad73eb19e
Semi <Semiharris3@gmail.com>
2022-03-09 11:32:01 -0500

    Created teh models

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const sequelize = require('./config/connection');
+const PORT = process.env.PORT || 3001;
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
